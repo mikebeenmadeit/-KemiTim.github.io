faa01d383dcb01532102d886dbd2def01bda48d8
mikebeenmadeit <107950050+mikebeenmadeit@users.noreply.github.com>
2023-12-27 13:50:52 -0800

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -3,6 +3,7 @@
     const items = document.querySelectorAll('.carousel-image');
     const totalItems = items.length;
     let currentIndex = 0;
+    let intervalId;
 
     function updateCarousel() {
         carousel.style.transform = `translateX(${-currentIndex * 300}px)`;
@@ -17,6 +18,17 @@
         currentIndex = (currentIndex - 1 + totalItems) % totalItems;
         updateCarousel();
     }
+
+   function startCarousel() {
+        intervalId = setInterval(nextSlide, 2000); // Change 2000 to the desired interval in milliseconds (e.g., 2000 for 2 seconds)
+    }
+
+    function stopCarousel() {
+        clearInterval(intervalId);
+    }
+
+    // Start the carousel when the page is loaded
+    document.addEventListener('DOMContentLoaded', startCarousel);
 
 
   // Automatic slideshow
